20432706ef54478ff3bb0e1a198066298b7b5491
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-10-17 16:43:04 -0300

    update-popup-img

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+const projetoTitulo = document.querySelectorAll("h3.projeto-titulo");
+
+
+for (let i=0; i < projetoTitulo.length; i++) {
+  const imagem = projetoTitulo[i].getAttribute('aria-controls');    
+  const imageHover = document.getElementById(imagem);
+  if (window.matchMedia("(min-width: 600px)").matches) {
+    projetoTitulo[i].addEventListener('mouseover', function(){
+      imageHover.style.display = 'inline-block';
+    });
+    projetoTitulo[i].addEventListener('mouseout', function(){
+      imageHover.style.display = 'none';
+    });
+  } 
+}
+
