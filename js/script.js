a0f3b20ae1137a9d8f2bf87be0f7c2839e76c431
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-11-13 16:43:16 -0300

    projetos-mobiles

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-
 const projetoTitulo = document.querySelectorAll(".projeto");
 
-
-for (let i=0; i < projetoTitulo.length; i++) {
-  const imagem = projetoTitulo[i].getAttribute('aria-controls');    
+for (let i = 0; i < projetoTitulo.length; i++) {
+  const imagem = projetoTitulo[i].getAttribute("aria-controls");
   const imageHover = document.getElementById(imagem);
   if (window.matchMedia("(min-width: 600px)").matches) {
-    projetoTitulo[i].addEventListener('mouseover', function(){
-      imageHover.style.display = 'inline-block';
+    projetoTitulo[i].addEventListener("mouseover", function () {
+      imageHover.style.display = "inline-block";
     });
-    projetoTitulo[i].addEventListener('mouseout', function(){
-      imageHover.style.display = 'none';
+    projetoTitulo[i].addEventListener("mouseout", function () {
+      imageHover.style.display = "none";
     });
-  } 
+  }
 }
-
